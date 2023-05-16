c4ba42f50737096197c348cfe12848c73ea62e1e
fernandoeiki <fernandoeiki@gmail.com>
2023-05-16 16:27:13 -0300

    Mudanca Tela

--- perSONA/helpForm.cs
+++ perSONA/helpForm.cs
@@ -16,11 +16,6 @@ namespace perSONA
         public helpForm()
         {
             InitializeComponent();
-            string w = System.Windows.SystemParameters.PrimaryScreenWidth.ToString();
-            string h = System.Windows.SystemParameters.PrimaryScreenHeight.ToString();
-            int x = Int32.Parse(w);
-            int y = Int32.Parse(h);
-            this.MinimumSize = new System.Drawing.Size((x * 1100) / 1920, (y * 700) / 1080);
             //this.MinimumSize = new System.Drawing.Size(1100,700);
         }
 
